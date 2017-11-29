ca77f9522d027499f1fa6df9603a3e551ca8dbcb
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2017-11-29 17:46:04 -0500

    Finish importing data from files

--- src/college/controller/FileHandler.java
+++ src/college/controller/FileHandler.java
@@ -13,7 +13,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 
-import college.model.Employee;
+import college.model.*;
 
 public class FileHandler {
 
@@ -28,10 +28,10 @@ public class FileHandler {
 		}
 		return new String(encoded);
 	}
-	
+
 	public JsonArray getJsonArr(String type) {
 		String content = readFile(type);
-		
+
 		JsonParser jsonParser = new JsonParser();
 		JsonObject jo = (JsonObject)jsonParser.parse(content);
 		return jo.getAsJsonArray(type);
@@ -44,7 +44,106 @@ public class FileHandler {
 		Gson gson = new Gson();
 		Type listType = new TypeToken<List<Employee>>(){}.getType();
 		list = gson.fromJson(jsonArr, listType);
-		
+
+		return list;
+	}
+
+	public List<Department> parseDepartmentJson() {
+		JsonArray jsonArr = getJsonArr("department");
+
+		List<Department> list = new ArrayList<Department>();
+		Gson gson = new Gson();
+		Type listType = new TypeToken<List<Department>>(){}.getType();
+		list = gson.fromJson(jsonArr, listType);
+
+		return list;
+	}
+
+	public List<DepartmentEmployee> parseDepartmentEmployeeJson() {
+		JsonArray jsonArr = getJsonArr("departmentEmployee");
+
+		List<DepartmentEmployee> list = new ArrayList<DepartmentEmployee>();
+		Gson gson = new Gson();
+		Type listType = new TypeToken<List<DepartmentEmployee>>(){}.getType();
+		list = gson.fromJson(jsonArr, listType);
+
+		return list;
+	}
+
+	public List<Program> parseProgramJson() {
+		JsonArray jsonArr = getJsonArr("program");
+
+		List<Program> list = new ArrayList<Program>();
+		Gson gson = new Gson();
+		Type listType = new TypeToken<List<Program>>(){}.getType();
+		list = gson.fromJson(jsonArr, listType);
+
+		return list;
+	}
+
+	public List<Course> parseCourseJson() {
+		JsonArray jsonArr = getJsonArr("course");
+
+		List<Course> list = new ArrayList<Course>();
+		Gson gson = new Gson();
+		Type listType = new TypeToken<List<Course>>(){}.getType();
+		list = gson.fromJson(jsonArr, listType);
+
+		return list;
+	}
+
+	public List<Classe> parseClasseJson() {
+		JsonArray jsonArr = getJsonArr("classe");
+
+		List<Classe> list = new ArrayList<Classe>();
+		Gson gson = new Gson();
+		Type listType = new TypeToken<List<Classe>>(){}.getType();
+		list = gson.fromJson(jsonArr, listType);
+
+		return list;
+	}
+
+	public List<Student> parseStudentJson() {
+		JsonArray jsonArr = getJsonArr("student");
+
+		List<Student> list = new ArrayList<Student>();
+		Gson gson = new Gson();
+		Type listType = new TypeToken<List<Student>>(){}.getType();
+		list = gson.fromJson(jsonArr, listType);
+
+		return list;
+	}
+
+	public List<StudentClass> parseStudentClassJson() {
+		JsonArray jsonArr = getJsonArr("studentClass");
+
+		List<StudentClass> list = new ArrayList<StudentClass>();
+		Gson gson = new Gson();
+		Type listType = new TypeToken<List<StudentClass>>(){}.getType();
+		list = gson.fromJson(jsonArr, listType);
+
+		return list;
+	}
+
+	public List<Schedule> parseScheduleJson() {
+		JsonArray jsonArr = getJsonArr("schedule");
+
+		List<Schedule> list = new ArrayList<Schedule>();
+		Gson gson = new Gson();
+		Type listType = new TypeToken<List<Schedule>>(){}.getType();
+		list = gson.fromJson(jsonArr, listType);
+
+		return list;
+	}
+
+	public List<GradeAux> parseGradeAuxJson() {
+		JsonArray jsonArr = getJsonArr("gradeAux");
+
+		List<GradeAux> list = new ArrayList<GradeAux>();
+		Gson gson = new Gson();
+		Type listType = new TypeToken<List<GradeAux>>(){}.getType();
+		list = gson.fromJson(jsonArr, listType);
+
 		return list;
 	}
 }
--- src/college/model/College.java
+++ src/college/model/College.java
@@ -15,107 +15,117 @@ public class College {
 	private static List<Student> students;
 	private static List<StudentClass> studentClasses;
 	private static List<Schedule> schedules;
-	
+	private static List<GradeAux> grades;
+
 	public void createCollege () {
 		createEmployees();
 		createDepartments();
 		createDepartmentEmployees();
 		createPrograms();
 		createCourses();
+		createClasses();
 		createStudents();
 		createStudentClasses();
 		createSchedules();
+		createGrades();
 	}
 	public void createEmployees () {
 		employees = fileHandler.parseEmployeeJson();
 	}
 	public void createDepartments () {
-		
+		departments = fileHandler.parseDepartmentJson();
 	}
 	public void createDepartmentEmployees () {
-			
+		departmentEmployees = fileHandler.parseDepartmentEmployeeJson();
 	}
 	public void createPrograms () {
-		
+		programs = fileHandler.parseProgramJson();
 	}
 	public void createCourses () {
-		
+		courses = fileHandler.parseCourseJson();
 	}
 	public void createClasses () {
-		
+		classes = fileHandler.parseClasseJson();
 	}
 	public void createAndInsertClasse () {
-		
+
 	}
 	public void createStudents () {
-		
+		students = fileHandler.parseStudentJson();
 	}
 	public void createStudentClasses () {
-			
+		studentClasses = fileHandler.parseStudentClassJson();
 	}
 	public void createAndInsertStudentClass () {
-		
+
 	}
 	public void createSchedules () {
-		
+		schedules = fileHandler.parseScheduleJson();
 	}
 	public void createAndInsertSchedule () {
-		
-	}
 
+	}
+	public void createGrades () {
+		grades = fileHandler.parseGradeAuxJson();
+	}
 	public static List<Employee> getEmployees() {
 		return employees;
 	}
 	public void setEmployees(List<Employee> employees) {
-		this.employees = employees;
+		College.employees = employees;
 	}
 	public static List<Department> getDepartments() {
 		return departments;
 	}
 	public void setDepartments(List<Department> departments) {
-		this.departments = departments;
+		College.departments = departments;
 	}
 	public static List<DepartmentEmployee> getDepartmentEmployees() {
 		return departmentEmployees;
 	}
 	public void setDepartmentEmployees(List<DepartmentEmployee> departmentEmployees) {
-		this.departmentEmployees = departmentEmployees;
+		College.departmentEmployees = departmentEmployees;
 	}
 	public static List<Program> getPrograms() {
 		return programs;
 	}
 	public void setPrograms(List<Program> programs) {
-		this.programs = programs;
+		College.programs = programs;
 	}
 	public static List<Course> getCourses() {
 		return courses;
 	}
 	public void setCourses(List<Course> courses) {
-		this.courses = courses;
+		College.courses = courses;
 	}
 	public static List<Classe> getClasses() {
 		return classes;
 	}
 	public void setClasses(List<Classe> classes) {
-		this.classes = classes;
+		College.classes = classes;
 	}
 	public static List<Student> getStudents() {
 		return students;
 	}
 	public void setStudents(List<Student> students) {
-		this.students = students;
+		College.students = students;
 	}
 	public static List<StudentClass> getStudentClasses() {
 		return studentClasses;
 	}
 	public void setStudentClasses(List<StudentClass> studentClasses) {
-		this.studentClasses = studentClasses;
+		College.studentClasses = studentClasses;
 	}
 	public static List<Schedule> getSchedules() {
 		return schedules;
 	}
 	public void setSchedules(List<Schedule> schedules) {
-		this.schedules = schedules;
+		College.schedules = schedules;
 	}
-	
+	public static List<GradeAux> getGrades() {
+		return grades;
+	}
+	public void setGrades(List<GradeAux> grades) {
+		College.grades = grades;
+	}
 }
