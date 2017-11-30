3e68b4671c89cdfcede7600dd0577decd11cf3f8
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2017-11-30 17:07:12 -0500

    Improve reports

--- src/college/controller/CollegeController.java
+++ src/college/controller/CollegeController.java
@@ -6,7 +6,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import college.model.Classe;
@@ -22,193 +21,196 @@ import college.model.Util;
 
 public class CollegeController {
 
-   //Instructors by classes and average student grades
-   public static void instructorsByClasses() {
-       String report = Util.padRight("Instructor", 18) + " | " + Util.padRight("Class Id",10) + " | " + Util.padRight("Course", 40) + " | Average Grade\n";
-       for (Classe c : College.getClasses()) {
-           if (getAverageGradeOfClasse(c) != 0) {
-               String instructor = c.getInstructor().getName();
-               int classId = c.getClasseId();
-               String course = c.getCourse().getName();
-               int average = getAverageGradeOfClasse(c);
-               report += Util.padRight(instructor, 18) + " | " + Util.padRight(classId, 10) + " | " + Util.padRight(course, 40) + " | " + average + "\n";
-           }
-       }
-       System.out.println(report);
-   }
-   
-   public static int getAverageGradeOfClasse(Classe classe) {
-       int sum = 0;;
-       int count = 0;;
-       for (StudentClass sc : College.getStudentClasses()) {
-           if (sc.getClasse().getClasseId() == classe.getClasseId()) {
-               if ((sc.getGradeAssig() != 0) && (sc.getGradeTest() != 0) && (sc.getGradeProject() != 0)) {
-                   sum += (sc.getGradeAssig() + sc.getGradeTest() + sc.getGradeProject()) / 3;
-                   count += 1;
-               }
-           }
-       }
-       if (count == 0) {
-           return 0;
-       }
-       return sum / count;
-   }
-   
-   //Number of students by country
-   public static void studentsByCountry() {
-       String report = Util.padRight("Country", 15) + "| Number of Students\n";
-       List<Country> countries = new ArrayList<Country>();
-       for (Student s : College.getStudents()) {
-           if (!countries.contains(s.getOriginCountry())) {
-               countries.add(s.getOriginCountry());
-           }
-       }
-       for (Country c : countries) {
-           int count = 0;;
-           for (Student s : College.getStudents()) {
-               if (s.getOriginCountry() == c) {
-                   count += 1;
-               }
-           }
-           report += Util.padRight(c.toString(), 15) + "| " + count + "\n";
-       }
-       System.out.println(report);
-   }
-   
-   //Top 3 courses with worst average grades
-   public static void coursesByWorstAverage() {
-       String report = Util.padRight("Course", 40) + "| Grade\n";
-       Map<Course, Integer> elements = new HashMap<Course, Integer>();
-       for (Course c : College.getCourses()) {
-           elements.put(c, getAverageGradeOfClassesInCourse(c));
-       }
-       Map<Course, Integer> treeMap = 
-    		     elements.entrySet().stream()
-    		    .sorted(Map.Entry.<Course, Integer> comparingByValue().reversed())
-    		    .collect(Collectors.toMap(Entry::getKey, Entry::getValue,
-    		                              (e1, e2) -> e1, LinkedHashMap::new));
-       //rodar o for só tres vezes
-       for (Map.Entry<Course, Integer> entry : treeMap.entrySet()) {
-    	   report += Util.padRight(entry.getKey().getName(), 40) + "| " + entry.getValue() +"\n";
-       }
-       //report += Util.padRight(Iterables.get(treeMap.entrySet(),0).getName(), 40) + "| " + treeMap[0] +"\n";
-       //report += Util.padRight(elements[1].0.getName(), 40) + "| \(elements[1].1)\n";
-       //report += Util.padRight(elements[2].0.getName(), 40) + "| \(elements[2].1)\n";
-       System.out.println(report);
-   }
-   
-   public static int getAverageGradeOfClassesInCourse(Course course) {
-       int sum = 0;
-       int count = 0;
-       for (StudentClass sc : College.getStudentClasses()) {
-           if (sc.getClasse().getCourse().getCourseId() == course.getCourseId()) {
-               if (sc.getGradeAssig() != 0 && sc.getGradeTest() != 0 && sc.getGradeProject() != 0) {
-                   sum += (sc.getGradeAssig() + sc.getGradeTest() + sc.getGradeProject()) / 3;
-                   count += 1;
-               }
-           }
-       }
-       if (count == 0) {
-           return 0;
-       }
-       return sum / count;
-   }
-   
-   
-   //Number of classes per instructor per weekday
-   public static void classesByInscructorsPerWeek() {
-       String report = Util.padRight("Instructor", 18) + " | " + Util.padRight("Weekday",12) + " | Number of classes\n";
-       Map<String, Integer> elements = new HashMap<String, Integer>();
-       
-       for (Employee i : College.getEmployees()) {
-    	   elements = schedulesPerInstructor(i);
-    	   for (Map.Entry<String, Integer> entry : elements.entrySet()) {
-               report += Util.padRight(i.getName(), 18) + " | " + Util.padRight(entry.getKey(), 12) + " | " + entry.getValue() + "\n";
-           }
-       }
-       System.out.println(report);
-   }
-   
-   public static Map<String, Integer> schedulesPerInstructor(Employee instructor) {
-	   Map<String, Integer> elements = new HashMap<String, Integer>();
-       for (Schedule s : College.getSchedules()) {
-           if (s.getClasse().getInstructor().getEmployeeId() == instructor.getEmployeeId()) {
-               int count = 1;
-               if (elements.containsKey(s.getWeekday())) {
-                   count = elements.get(s.getWeekday()) + 1;
-               }
-               elements.put(s.getWeekday(), count);
-           }
-       }
-       return elements;
-   }
-   
-   
-   //Top student with best average grade by program
-   public static void studentsByBestAverage() {
-       String report = Util.padRight("Program", 20) + "| " + Util.padRight("Student", 20) + "| Best Average Grade\n";
-       for (Program p : College.getPrograms()) {
-    	   Map<Student, Integer> elements = new HashMap<Student, Integer>();
-    	   elements = getAverageGradeOfEachSudentInProgram(p);
-           Map<Student, Integer> treeMap = 
-      		     elements.entrySet().stream()
-      		    .sorted(Map.Entry.<Student, Integer> comparingByValue().reversed())
-      		    .collect(Collectors.toMap(Entry::getKey, Entry::getValue,
-      		                              (e1, e2) -> e1, LinkedHashMap::new));
-           //rodar o for só uma vez
-           for (Map.Entry<Student, Integer> entry : treeMap.entrySet()) {
-        	   report += Util.padRight(p.getName(), 20) + "| " + Util.padRight(entry.getKey().getName(), 20) + "| " + entry.getValue() + "\n";
-        	   //report += Util.padRight(p.getName(), 20) + "| " + Util.padRight(elements[0].0.getName(), 20) + "| " + elements[0].1 + "\n";
-           }
-       }
-       System.out.println(report);
-   }
+	//Instructors by classes and average student grades
+	public static void instructorsByClasses() {
+		String report = Util.padRight("Instructor", 18) + " | " + Util.padRight("Class Id",10) + " | "
+				+ Util.padRight("Course", 42) + " | Average Grade\n";
+		for (Classe c : College.getClasses()) {
+			if (getAverageGradeOfClasse(c) != 0) {
+				String instructor = c.getInstructor().getName();
+				int classId = c.getClasseId();
+				String course = c.getCourse().getName();
+				int average = getAverageGradeOfClasse(c);
+				report += Util.padRight(instructor, 18) + " | " + Util.padRight(classId, 10) + " | "
+						+ Util.padRight(course, 42) + " | " + average + "\n";
+			}
+		}
+		System.out.println(report);
+	}
 
-   public static Map<Student, Integer> getAverageGradeOfEachSudentInProgram(Program program) {
-       Map<Student, Integer> elements = new HashMap<Student, Integer>();
-       for (Student s : College.getStudents()) {
-           Student student = new Student();
-           int sum = 0;
-           int count = 0;
-           for (StudentClass sc : College.getStudentClasses()) {
-               if (sc.getClasse().getCourse().getProgram().getProgramId() == program.getProgramId()
-                   && sc.getStudent().getStudentId() == s.getStudentId()) {
-                   student = s;
-                   if (sc.getGradeAssig() != 0 && sc.getGradeTest() != 0 && sc.getGradeProject() != 0) {
-                       sum += (sc.getGradeAssig() + sc.getGradeTest() + sc.getGradeProject()) / 3;
-                       count += 1;
-                   }
-                   
-               }
-           }
-           if (student != null) {
-               int value = 0;
-               if (count != 0) {
-                   value = sum / count;
-               }
-               elements.put(student, value);
-           }
-       }
-       return elements;
-   }
-   
-   public static List<Classe> getClassesOfInstructor(Employee instructor) {
-	   List<Classe> list = new ArrayList<>();
-	   for (Classe c: College.getClasses()) {
-		   if (c.getInstructor().getEmployeeId() == instructor.getEmployeeId()) {
-			   list.add(c);
-		   }
-	   }
-	   return list;
-   }
-   
-   public static List<StudentClass> getStudentClassesOfClasse(Classe classe) {
-	   List<StudentClass> list = new ArrayList<>();
-	   for (StudentClass sc: College.getStudentClasses()) {
-		   if (sc.getClasse().getClasseId() == classe.getClasseId()) {
-			   list.add(sc);
-		   }
-	   }
-	   return list;
-   }
+	public static int getAverageGradeOfClasse(Classe classe) {
+		int sum = 0;;
+		int count = 0;;
+		for (StudentClass sc : College.getStudentClasses()) {
+			if (sc.getClasse().getClasseId() == classe.getClasseId()) {
+				if ((sc.getGradeAssig() != 0) && (sc.getGradeTest() != 0) && (sc.getGradeProject() != 0)) {
+					sum += (sc.getGradeAssig() + sc.getGradeTest() + sc.getGradeProject()) / 3;
+					count += 1;
+				}
+			}
+		}
+		if (count == 0) {
+			return 0;
+		}
+		return sum / count;
+	}
+
+	//Number of students by country
+	public static void studentsByCountry() {
+		String report = Util.padRight("Country", 15) + "| Number of Students\n";
+		List<Country> countries = new ArrayList<Country>();
+		for (Student s : College.getStudents()) {
+			if (!countries.contains(s.getOriginCountry())) {
+				countries.add(s.getOriginCountry());
+			}
+		}
+		for (Country c : countries) {
+			int count = 0;;
+			for (Student s : College.getStudents()) {
+				if (s.getOriginCountry() == c) {
+					count += 1;
+				}
+			}
+			report += Util.padRight(c.toString(), 15) + "| " + count + "\n";
+		}
+		System.out.println(report);
+	}
+
+	//Top 3 courses with worst average grades
+	public static void coursesByWorstAverage() {
+		String report = Util.padRight("Course", 42) + "| Grade\n";
+		Map<Course, Integer> elements = new HashMap<Course, Integer>();
+		for (Course c : College.getCourses()) {
+			elements.put(c, getAverageGradeOfClassesInCourse(c));
+		}
+		Map<Course, Integer> treeMap = 
+				elements.entrySet().stream()
+				.sorted(Map.Entry.<Course, Integer> comparingByValue().reversed())
+				.collect(Collectors.toMap(Entry::getKey, Entry::getValue,
+						(e1, e2) -> e1, LinkedHashMap::new));
+		int count = 3;
+		for (Map.Entry<Course, Integer> entry : treeMap.entrySet()) {
+			if (count > 0) {
+				count--;
+			} else {
+				break;
+			}
+			report += Util.padRight(entry.getKey().getName(), 42) + "| " + entry.getValue() +"\n";
+		}
+		System.out.println(report);
+	}
+
+	public static int getAverageGradeOfClassesInCourse(Course course) {
+		int sum = 0;
+		int count = 0;
+		for (StudentClass sc : College.getStudentClasses()) {
+			if (sc.getClasse().getCourse().getCourseId() == course.getCourseId()) {
+				if (sc.getGradeAssig() != 0 && sc.getGradeTest() != 0 && sc.getGradeProject() != 0) {
+					sum += (sc.getGradeAssig() + sc.getGradeTest() + sc.getGradeProject()) / 3;
+					count += 1;
+				}
+			}
+		}
+		if (count == 0) {
+			return 0;
+		}
+		return sum / count;
+	}
+
+
+	//Number of classes per instructor per weekday
+	public static void classesByInscructorsPerWeek() {
+		String report = Util.padRight("Instructor", 18) + " | " + Util.padRight("Weekday",12) + " | Number of classes\n";
+		Map<String, Integer> elements = new HashMap<String, Integer>();
+
+		for (Employee i : College.getEmployees()) {
+			elements = schedulesPerInstructor(i);
+			for (Map.Entry<String, Integer> entry : elements.entrySet()) {
+				report += Util.padRight(i.getName(), 18) + " | " + Util.padRight(entry.getKey(), 12) + " | " + entry.getValue() + "\n";
+			}
+		}
+		System.out.println(report);
+	}
+
+	public static Map<String, Integer> schedulesPerInstructor(Employee instructor) {
+		Map<String, Integer> elements = new HashMap<String, Integer>();
+		for (Schedule s : College.getSchedules()) {
+			if (s.getClasse().getInstructor().getEmployeeId() == instructor.getEmployeeId()) {
+				int count = 1;
+				if (elements.containsKey(s.getWeekday())) {
+					count = elements.get(s.getWeekday()) + 1;
+				}
+				elements.put(s.getWeekday(), count);
+			}
+		}
+		return elements;
+	}
+
+
+	//Top student with best average grade by program
+	public static void studentsByBestAverage() {
+		String report = Util.padRight("Program", 20) + "| " + Util.padRight("Student", 20) + "| Best Average Grade\n";
+		for (Program p : College.getPrograms()) {
+			Map<Student, Integer> elements = new HashMap<Student, Integer>();
+			elements = getAverageGradeOfEachSudentInProgram(p);
+			Map<Student, Integer> treeMap = 
+					elements.entrySet().stream()
+					.sorted(Map.Entry.<Student, Integer> comparingByValue().reversed())
+					.collect(Collectors.toMap(Entry::getKey, Entry::getValue,
+							(e1, e2) -> e1, LinkedHashMap::new));
+			for (Map.Entry<Student, Integer> entry : treeMap.entrySet()) {
+				report += Util.padRight(p.getName(), 20) + "| " + Util.padRight(entry.getKey().getName(), 20) + "| " + entry.getValue() + "\n";
+				break;
+			}
+		}
+		System.out.println(report);
+	}
+
+	public static Map<Student, Integer> getAverageGradeOfEachSudentInProgram(Program program) {
+		Map<Student, Integer> elements = new HashMap<Student, Integer>();
+		for (Student s : College.getStudents()) {
+			Student student = new Student();
+			int sum = 0;
+			int count = 0;
+			for (StudentClass sc : College.getStudentClasses()) {
+				if (sc.getClasse().getCourse().getProgram().getProgramId() == program.getProgramId()
+						&& sc.getStudent().getStudentId() == s.getStudentId()) {
+					student = s;
+					if (sc.getGradeAssig() != 0 && sc.getGradeTest() != 0 && sc.getGradeProject() != 0) {
+						sum += (sc.getGradeAssig() + sc.getGradeTest() + sc.getGradeProject()) / 3;
+						count += 1;
+					}
+
+				}
+			}
+			if (student != null) {
+				int value = 0;
+				if (count != 0) {
+					value = sum / count;
+				}
+				elements.put(student, value);
+			}
+		}
+		return elements;
+	}
+
+	public static List<Classe> getClassesOfInstructor(Employee instructor) {
+		List<Classe> list = new ArrayList<>();
+		for (Classe c: College.getClasses()) {
+			if (c.getInstructor().getEmployeeId() == instructor.getEmployeeId()) {
+				list.add(c);
+			}
+		}
+		return list;
+	}
+
+	public static List<StudentClass> getStudentClassesOfClasse(Classe classe) {
+		List<StudentClass> list = new ArrayList<>();
+		for (StudentClass sc: College.getStudentClasses()) {
+			if (sc.getClasse().getClasseId() == classe.getClasseId()) {
+				list.add(sc);
+			}
+		}
+		return list;
+	}
 }
